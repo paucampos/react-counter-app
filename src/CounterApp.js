a5fa1de3c433a28d058ef1106af2224295e0ba50
Pau Campos <pau.campos89@gmail.com>
2021-09-28 12:44:40 -0300

    substract + reset - Final

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,14 +2,16 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 
 // Functional Component
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10}) => {
   // Hook
-  const [counter, setCounter] = useState(0); // retorna un [] con dos valores.
+  const [counter, setCounter] = useState(value); // retorna un [] con dos valores.
 
   // HandleAdd
-  const handleAdd = () => {
-    setCounter(counter + 1);
-  };
+  const handleAdd = () => setCounter(counter + 1);
+
+  const handleSubstract = () => setCounter(counter - 1);
+
+  const handleReset = () => setCounter(value);
 
   return (
     <>
@@ -17,6 +19,8 @@ const CounterApp = ({ value }) => {
       <h2> {counter} </h2>
 
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleSubstract}>-1</button>
     </>
   );
 };
